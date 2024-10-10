441c02af80fe75c0b4e42055534618c8a59c648c
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-10 15:26:01 +0200

    create numbers var and take element by Id from html

--- main.js
+++ main.js
@@ -11,8 +11,17 @@ Dividete in piccoli problemi la consegna.
 Individuate gli elementi di cui avete bisogno per realizzare il programma.
 Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"*/
 
-let numbers = getRandomNumber(1, 100);
-console.log(numbers);
+
+
+
+//creo una variabile dove contenere i numeri generati
+let randomNumbers = getRandomNumber(1, 100);
+console.log(randomNumbers);
+
+//prendo l'elemento in cui inserirò i numeri nell'html con getElementById
+let numbers = document.getElementById('numbers');
+
+numbers.innerHTML = randomNumbers;
 
 
 //Creo una funzione che mi permetta di visualizzare in pagina 5 numeri casuali
